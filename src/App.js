5f25c54bdb2af5946bfaca2311b0ae72cd4f45f7
uzFer <109243682+uzFer@users.noreply.github.com>
2022-11-09 20:21:49 -0500

    trying link stuff

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
+import { Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
           <p><span>Hi, </span>what would you like to do today?
           </p>
           <div className="button-container">
+
         <div className="main-btn">
-        <a
-          className="main-btn-content"
-          href="SignLog.js">
-          Sign Up
-        </a>
+        <ul>
+        <li>
+          {/* Endpoint to route to Home component */}
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          {/* Endpoint to route to About component */}
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          {/* Endpoint to route to Contact Us component */}
+          <Link to="/contactus">Contact Us</Link>
+        </li>
+      </ul>
         </div>
+
+        
         <div className="main-btn">
         <a
           className="main-btn-content"
