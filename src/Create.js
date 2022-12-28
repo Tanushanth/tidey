7cecc457ea6020e34a1b6284c0ad2b21f8405212
uzFer <109243682+uzFer@users.noreply.github.com>
2022-12-28 01:06:12 -0500

    made post request

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [courseCode, setCourseCode] = useState('');
+  const [courseName, setCourseName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const courses = { courseCode, courseName };
+
+    fetch('http://localhost:8000/courses', {
+      method: 'POST',
+      headers: { "Content-Type": "applications/json"},
+      body: JSON.stringify(courses)
+    }).then(() => {
+      console.log('new course added');
+    })
+  }
 
   return (
     <div className="App">
@@ -6,12 +23,25 @@ const Create = () => {
         <h3>Add a new course</h3>
 
         <div className="create">
-          <form>
+          <form onSubmit={ handleSubmit }>
             <label>Course Code:</label>
-            <input type="text" required/>
+            <input 
+              type="text" 
+              required
+              value={ courseCode }
+              onChange={(e) => setCourseCode(e.target.value)}
+            />
+
             <label>Course Name:</label>
-            <input type="text" required/>
+            <input 
+              type="text" 
+              required
+              value={ courseName }
+              onChange={(e) => setCourseName(e.target.value)}
+            />
+
             <button>Add course</button>
+
           </form>
         </div>
 
