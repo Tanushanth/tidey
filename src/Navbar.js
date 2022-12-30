f770dadaad5f3a7f84258c552a70b1aa58b544d7
Tanushanth <79293445+Tanushanth@users.noreply.github.com>
2022-12-30 16:33:01 -0500

    Revert "small edits to style"
    This reverts commit b24668aeead2d3b071623b7159bf55f8d476ef4f.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,28 +1,5 @@
 import { Link } from "react-router-dom";
-
-
-const hamburgerMenu = document.querySelector(".navbar__toggle");
-const menuLinks = document.querySelector(".navbar__menu");
-const menu = document.querySelector(".navbar__menu");
-
-
-// Display Mobile Menu
-const mobileMenu = () => {
-  menu.classList.toggle("active");
-  menuLinks.classList.toggle("active");
-};
-
-hamburgerMenu.addEventListener("click", () => {
-  hamburgerMenu.classList.toggle("active");
-  menuLinks.classList.toggle("active");
-})
-
-document.querySelectorAll(".navbar__links").forEach(n => n.addEventListener("click", () => {
-  hamburgerMenu.classList.remove("active");
-  menuLinks.classList.remove("active");
-}))
-
-
+ 
 const Navbar = () => {
     return ( 
         <nav class="navbar">
