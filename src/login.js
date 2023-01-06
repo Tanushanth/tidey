db4951ab82adcaff9ecb423434731ec22adfe119
uzFer <109243682+uzFer@users.noreply.github.com>
2023-01-06 14:01:39 -0500

    re-styling the login and signup

--- src/login.js
+++ src/login.js
@@ -63,27 +63,44 @@ const Login = () => {
 
   return (
     <div className="App">
+       <div className="background-style">
+      {/* Waves Container*/}
+      <div>
+            <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto" >
+              <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+              </defs>
+              <g class="parallax">
+                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(120,180,231,0.3)" />
+                <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(120,180,231,0.5)" />
+                <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(120,180,231,0.7)" />
+                <use xlinkHref="#gentle-wave" x="48" y="7" fill="rgba(120,180,255,1.000)" />
+              </g>
+            </svg>
+          </div>
       <header className="App-header">
           <div className="textbox">
             <p className="textbox-heading" style={{ fontWeight: "bold" }}>Log in to your account</p>
-            <button onClick={signInWithGoogle}>Sign in with Google</button>
+            <button onClick={ signInWithGoogle }>Sign in with Google</button>
 
-            <form onSubmit={login}>
-              <label style={headingStyle} >Email: </label>
-              <input type="text" 
+            <form onSubmit={ login }>
+              <label style={ headingStyle } >Email: </label>
+              <input type="info" 
                 placeholder="Email..."
-                onChange={(e)=> {setLoginEmail(e.target.value)}}
+                onChange={(e)=> { setLoginEmail(e.target.value) }}
                 className="form"/>
 
               <label style={headingStyle} >Password: </label>
-              <input type="text" 
+              <input type="info" 
                 placeholder="Password..."
-                onChange={(e)=> {setLoginPassword(e.target.value)}}
+                onChange={(e)=> { setLoginPassword(e.target.value) }}
                 className="form"/>
               <button>Enter</button>
             </form>
           </div>
       </header>
+    </div>
     </div>
   );
 }
--- src/signup.js
+++ src/signup.js
@@ -39,28 +39,46 @@ const SignUp = () => {
   }
 
   return (
+    
     <div className="App">
+      <div className="background-style">
+      {/* Waves Container*/}
+      <div>
+            <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto" >
+              <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+              </defs>
+              <g class="parallax">
+                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(120,180,231,0.3)" />
+                <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(120,180,231,0.5)" />
+                <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(120,180,231,0.7)" />
+                <use xlinkHref="#gentle-wave" x="48" y="7" fill="rgba(120,180,255,1.000)" />
+              </g>
+            </svg>
+          </div>
       <header className="App-header">
           <div className="textbox">
             <p className="textbox-heading" style={{ fontWeight: "bold" }}>Create a new Tidey account</p>
 
-            <form onSubmit={register}>
-              <label style={headingStyle}>Enter an Email:</label>
-              <input type="text" 
+            <form onSubmit={ register }>
+              <label style={ headingStyle }>Enter an Email:</label>
+              <input type="info" 
                 placeholder="Email..." 
-                onChange={(e)=> {setRegisterEmail(e.target.value)}}
+                onChange={(e)=> { setRegisterEmail(e.target.value) }}
                 className="form" />
               
-              <label style={headingStyle} >Enter a Password:</label>
-              <input type="text" 
+              <label style={ headingStyle } >Enter a Password:</label>
+              <input type="info" 
                 placeholder="Password..." 
-                onChange={(e)=> {setRegisterPassword(e.target.value)}}
+                onChange={(e)=> { setRegisterPassword(e.target.value)} }
                 className="form" />
 
               <button>Create</button>
             </form>
           </div>
       </header>
+      </div>
     </div>
   );
 }
